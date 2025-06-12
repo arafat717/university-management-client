cecb48bb1ebcd3d4c8a79a7878c57633976584a6
arafat717 <arafatjibon33@gmail.com>
2025-06-12 22:42:42 +0600

    added login redirect and logout

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { router } from "./routes/routes.tsx";
 import { Provider } from "react-redux";
 import { persistor, store } from "./redux/features/store.ts";
 import { PersistGate } from "redux-persist/integration/react";
+import { Toaster } from "sonner";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -13,5 +14,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <PersistGate loading={null} persistor={persistor}></PersistGate>
       <RouterProvider router={router}></RouterProvider>
     </Provider>
+    <Toaster></Toaster>
   </React.StrictMode>
 );
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,27 +4,37 @@ import React from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { useLoginMutation } from "../redux/features/auth/authApi";
 import { useAppDispatch } from "../redux/features/hooks";
-import { setUser } from "../redux/features/auth/authSlice";
+import { setUser, TUser } from "../redux/features/auth/authSlice";
 import { verifyToken } from "../utils/verifyToken";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
 type TLogin = {
   id: string;
   password: string;
 };
 
 const Login = () => {
+  const navigate = useNavigate();
   const dispatch = useAppDispatch();
   const { register, handleSubmit } = useForm<TLogin>();
   const [login, { data, error: any }] = useLoginMutation();
   console.log(data);
   const onSubmit: SubmitHandler<TLogin> = async (data) => {
+    const toastId = toast.loading("Logging in");
     const authInfo = {
       id: data.id,
       password: data.password,
     };
 
-    const res = await login(authInfo).unwrap();
-    const user = verifyToken(res.data.accesstoken);
-    dispatch(setUser({ user: user, token: res.data.accesstoken }));
+    try {
+      const res = await login(authInfo).unwrap();
+      const user = verifyToken(res.data.accesstoken) as TUser;
+      dispatch(setUser({ user: user, token: res.data.accesstoken }));
+      navigate(`/${user.role}/dashboard`);
+      toast.success("Logging in", { id: toastId, duration: 2000 });
+    } catch (err) {
+      toast.error("Something went wrong!", { id: toastId, duration: 2000 });
+    }
   };
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
